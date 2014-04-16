8f275e3684547bd5866d3964d9f05f5bf62dcd7e
JL <world_jadi@hotmail.com>
2014-04-16 21:33:03 +0700

    add file

---
+++ src/resource.js
@@ -0,0 +1,21 @@
+
+var s_Background = "res/images/background.png";
+var s_Floor1 = "res/images/floor1.png";
+var s_Gum = "res/images/gum.png";
+var s_Kyoda = "res/images/kyoda.png";var g_resources = [
+    //image
+    {src:s_Background},
+    {src:s_Floor1},
+    {src:s_Gum},
+    {src:s_Kyoda}
+ 
+    //plist
+ 
+    //fnt
+ 
+    //tmx
+ 
+    //bgm
+ 
+    //effect
+];
